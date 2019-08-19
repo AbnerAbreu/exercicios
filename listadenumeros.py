3978db0799a9d9b35944deb98e63fbd2ba95eb52
AbnerAbreu <abnerdrift94@gmail.com>
2019-08-19 16:38:11 -0300

    Adicionar exercicio/lista-par-impar

---
+++ listadenumeros.py
@@ -0,0 +1,18 @@
+listas = [1,2,3,4,5,6,7,8,9,0]
+
+for lista in listas:
+
+def par(lista):
+    if lista % 2 is 0:
+    print(lista)
+
+def impar(lista):
+    elif lista % 2 != 0:
+    print (lista)
+
+par()
+impar()
+
+
+
+
